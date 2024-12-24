2d22afdb4a63ecd6d447ed4587999702569dc8d4
Tomas Gallucci <professortom@gmail.com>
2024-12-23 18:31:01 -0600

    add unit tests for `getGroqBot()`

--- src/modules/__test__/bots.test.js
+++ src/modules/__test__/bots.test.js
@@ -1,5 +1,6 @@
-import {getOpenAIBot} from "../bots.js";
+import {getGroqBot, getOpenAIBot} from "../bots.js";
 import OpenAI from "openai";
+const Groq = require("groq-sdk"); // Jest can't pull this in with "import"
 
 describe("bots.js unit tests", () => {
     const originalEnv = process.env;
@@ -37,6 +38,38 @@ describe("bots.js unit tests", () => {
 
                     const openAIBot = getOpenAIBot();
                     expect(openAIBot.apiKey).toEqual(fakeApiKey);
+                })
+            })
+        });
+    });
+
+    describe("getGroqBot", () => {
+        describe("when api key is", () => {
+            describe('empty string', () => {
+                it("should return 'undefined'", () => {
+                    process.env.GROQ_API_KEY = "";
+
+                    const openaiBot = getGroqBot();
+                    expect(openaiBot).toEqual(undefined);
+                })
+            })
+
+            describe('NOT empty string', () => {
+                it("should return an instantiated GroqBot", () => {
+                    process.env.GROQ_API_KEY = "fakeGroqApiKey";
+
+                    const groqBot = getGroqBot();
+                    console.log(groqBot.constructor.name)
+                    expect(groqBot).toBeInstanceOf(Groq);
+                });
+
+                it("sends correct environment variable", () => {
+                    const fakeApiKey = "fakeApiKey";
+                    process.env.GROQ_API_KEY = fakeApiKey;
+
+                    const groqBot = getGroqBot();
+                    // console.log(JSON.stringify(groqBot))
+                    expect(groqBot.apiKey).toEqual(fakeApiKey);
                 })
             })
         });
